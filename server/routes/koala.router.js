165d2bb19676a7075c0e36fff138bfd637ce17c4
John Idso <john.d.idso@gmail.com>
2021-07-01 20:32:07 -0500

    think functionality to move transfer status to Y is ready. still need to test

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,4 +1,6 @@
+const { Router } = require('express');
 const express = require('express');
+const { Pool } = require('pg');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
@@ -11,7 +13,20 @@ const koalaRouter = express.Router();
 
 
 // PUT
+koalaRouter.put(':/id', (req, res) => {
+    const koalaId = req.params.id;
+    let queryText = `UPDATE "koalas" SET readyForTransfer=Y WHERE id=$1;`;
 
+    Pool.query(queryText, [koalaId])
+    .then(dbResponse => {
+        console.log('Updated Koala Status:', dbResponse.rowCount);
+        res.sendStatus(202);
+    })
+    .catch(error => {
+        console.log('There was an error updating the record', error);
+        res.sendStatus(500);
+    });
+})
 
 // DELETE
 
