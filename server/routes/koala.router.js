ea8ef9bebc889ae9604efa164be8799ac1803e09
Hann1234 <christopherjhannigan@gmail.com>
2021-07-01 22:09:33 -0500

    added POST route to koala.router.js

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,5 +1,6 @@
 const pool = require('../modules/pool');
 const express = require('express');
+const { Router } = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
@@ -9,7 +10,21 @@ const koalaRouter = express.Router();
  
 
 // POST
-
+router.post('/', (req, res) => {
+    const newKoala = req.body;
+    const queryText = `
+    INSERT INTO koalas ("name", "gender", "age", "readyForTransfer", "notes")
+    VALUES ('${newKoala.name}', '${newKoala.gender}', '${newKoala.age}', '${newKoala.readyForTransfer}', '${newKoala.notes}');
+    `;
+    pool.query(queryText)
+        .then((result) => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log(`Error making query ${queryText}`, err);
+            res.sendStatus(500);
+        });
+});
 
 // PUT
 
