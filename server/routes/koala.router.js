70c56fcdda6eccd04a5646493992a02d875db9cb
Meghan Cleland <mplscleland@gmail.com>
2021-07-01 19:26:21 -0500

    added get to koalarouterjs

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,12 +1,26 @@
 const pool = require('../modules/pool');
 const express = require('express');
+const { Router } = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
 
 
 // GET
- 
+
+koalaRouter.get('/', (req, res) => {
+    //create variable to hold SQL query
+    let queryText = 'SELECT * FROM "koalas";';
+    //make SQL query to DB...
+    pool.query(queryText)
+    .then(result => {
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('Error trying to get the koala from Postgres', error);
+        res.sendStatus(500);
+    });
+});
 
 // POST
 
